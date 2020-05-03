41f90707bee85a2204b29f1747ef86a9f5390ad6
Vincenvalentine <genz18keyz@gmail.com>
2020-05-03 23:33:04 +0700

    Create Header Commponet and put  to landing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+
 import 'assets/scss/style.scss';
+import LandingPages from 'pages/LandingPages';
 
 
 function App() {
   return (
     <div className="App">
-      hello lucky!
+      <Router>
+        <Route path="/" component={LandingPages}></Route>
+      </Router>
     </div>
   );
 }
---
+++ src/parts/Header.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import Button from 'elements/Button'
+import BrandIcon from 'parts/IconText'
+
+export default function Header(props) {
+
+    const getNavLinkClass = path => {
+        return props.location.pathname === path ? " active" : "";
+    }
+
+    return (
+        <header className="spacing-sm">
+            <div className="container">
+                <nav className="navbar navbar-expand-lg navbar-light">
+                    <BrandIcon />
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+                    
+                    <div className="collapse navbar-collapse">
+                        <ul className="navbar-nav ml-auto">
+                            <li className={`nav-item${getNavLinkClass("/")}`}>
+                                <Button className="nav-link" type="link" href="/">
+                                    Home
+                           </Button>
+                            </li>
+
+                            <li className={`nav-item${getNavLinkClass("/browse-by")}`}>
+                                <Button className="nav-link" type="link" href="/browse-by">
+                                    Browse By
+                           </Button>
+                            </li>
+
+                            <li className={`nav-item${getNavLinkClass("/stories")}`}>
+                                <Button className="nav-link" type="link" href="/stories">
+                                    Stories
+                           </Button>
+                            </li>
+
+                            <li className={`nav-item${getNavLinkClass("/agent")}`}>
+                                <Button className="nav-link" type="link" href="/agent">
+                                    Agent
+                           </Button>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </header>
+    )
+}
