c141b46292bcdaf7f2d370be48d858b2dc6a399e
Vincenvalentine <genz18keyz@gmail.com>
2020-05-08 12:31:46 +0700

    Finished Category part
    category part

--- src/pages/LandingPages.js
+++ src/pages/LandingPages.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from 'parts/Header';
 import Hero from 'parts/Hero';
 import MostPicked from 'parts/MostPicked'
+import Categories from 'parts/Categories'
 
 import landingPage from 'json/landingPage.json';
 
@@ -16,6 +17,7 @@ export default class LandingPages extends Component {
                 <Header {...this.props}></Header>
                 <Hero refMostPicked={this.refMostPicked} data={landingPage.hero}></Hero>
                 <MostPicked refMostPicked={this.refMostPicked} data={landingPage.mostPicked}></MostPicked>
+                <Categories data={landingPage.categories}></Categories>
             </>
         );
     }
